807d6d0c0a50eed275a1d51874fcd06ec51f3fd3
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-24 12:42:22 -0300

    Create friendship model and add relationship between Users and friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :friends, through: :friendships, source: :friend
 end
