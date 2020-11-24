ce50740df70153352a2f11775583121b095b0a86
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-24 20:07:36 -0300

    Change friendships variable to invited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
     #@friendships = Friendship.find_by(user_id: current_user, friend_id: @user.id)
-    @friendships = current_user.friends_join(current_user.id)
-    @friendships = @friendships[:invited].find_by(friend_id: @user.id)
+    @invited = current_user.friends_join(current_user.id)
+    @invited = @invited[:invited].find_by(friend_id: @user.id)
   end
 end
