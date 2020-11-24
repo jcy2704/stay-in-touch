368f51a2750c093d6bace523a11c652754972458
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-24 18:36:46 -0500

    Refactor invite system in user's show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    #@friendships = Friendship.find_by(user_id: current_user, friend_id: @user.id)
-    @invited = current_user.friends_join(current_user.id)
-    @invited = @invited[:invited].find_by(friend_id: @user.id)
-    @was_invited = current_user.friends_join(current_user.id)
-    @was_invited = @was_invited[:was_invited].find_by(user_id: @user.id)
+    @invited = Friendship.find_by(user_id: current_user.id, friend_id: @user.id)
+    @was_invited = Friendship.find_by(user_id: @user.id, friend_id: current_user.id)
   end
 end
