3cabd04b5963dc2be5fdf0799680ceb29ce7ac43
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-26 17:09:27 -0300

    Change friendship structure with composite primary keys

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,17 @@
 class Friendship < ApplicationRecord
+  before_save :find_lesser_id
+
+  self.primary_keys = :user_id, :friend_id
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def find_lesser_id
+    if user_id > friend_id
+      puts "It entered here?"
+      id = user_id
+      user_id! = friend_id
+      friend_id! = id
+      puts user_id, friend_id
+    end
+  end
 end
