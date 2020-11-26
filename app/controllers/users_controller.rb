1fa3ada9cbd69d61571a6154f18620ed0db7075d
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-26 17:22:20 -0500

    Refactor show profile invite logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @invited = Friendship.find_by(user_id: current_user.id, friend_id: @user.id)
-    @was_invited = Friendship.find_by(user_id: @user.id, friend_id: current_user.id)
-
-    @all_friends = Friendship.all.where(user_id: @user.id, status: true)
-    @all_friends = @all_friends.or(Friendship.all.where(friend_id: @user.id, status: true))
-
     @invites = @user.friends_join(@user.id)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,23 +5,39 @@ module UsersHelper
 
   private
 
-  def none_friendship(user)
+  def index_friendship(user)
     invites = current_user.friends_join(current_user.id)
     if invites[:was_invited].any? && !invites[:was_invited].find_by(user_id: user.id).nil?
       if invites[:was_invited].find_by(user_id: user.id).status == false
-        render partial: 'accept_form', locals: { invites: invites, user: user }
+        render partial: 'accept_form', locals: { friendreq: invites[:was_invited].find_by(user_id: user.id) }
       else
         label_tag 'Friends'
       end
     else
-      button_to  'Invite', friendships_path(friend_id: user.id)
+      button_to 'Invite', friendships_path(friend_id: user.id)
+    end
+  end
+
+  def show_friend(user)
+    invited = Friendship.find_by(user_id: current_user.id, friend_id: user.id)
+    was_invited = Friendship.find_by(user_id: user.id, friend_id: current_user.id)
+    if !invited.nil? && invited.status == false
+      label_tag 'Pending'
+    elsif !was_invited.nil? && was_invited.status == false
+      render partial: 'accept_form', locals: { friendreq: was_invited }
+    elsif was_invited.nil? && invited.nil?
+      button_to 'invite', friendships_path(user_id: current_user.id, friend_id: user.id)
+    else
+      label_tag 'Friends'
     end
   end
 
   def not_current_user(user)
     friendships = current_user.friendships
+    show_friend(user) if current_page?(user_path(user.id))
+
     if friendships.empty? || friendships.find_by(friend_id: user.id).nil?
-      none_friendship(user)
+      index_friendship(user)
     elsif friendships.find_by(friend_id: user.id).status == false
       label_tag 'Pending'
     else
