36cbdd42ed37fa19e7ef3a37a021a583f77c155f
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-27 14:33:28 -0300

    Add pending and requested friends snippets

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,14 @@ class User < ApplicationRecord
   has_many :confirmed_friendships, -> { where status: true }, class_name: "Friendship"
   has_many :friends, through: :confirmed_friendships
 
+  has_many :pending_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  has_many :inverted_friendships, -> { where status: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friend_requests, through: :inverted_friendships
+
   has_many :f_posts, through: :friends, source: :posts
+
   def friends_join(param)
     i_invited = Friendship.where('user_id = ?', param)
     i_was_invited = Friendship.where('friend_id = ?', param)
