40689c52beaea71cbd2aee3a5327d7bb0567209c
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-27 14:13:26 -0300

    Remove duplicated names on User's friend's list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,5 @@ class UsersController < ApplicationController
     ids << current_user.id
     @posts = @user.posts.ordered_by_most_recent if ids.include?(@user.id)
     @invites = Friendship.all.where(user_id: @user.id, status: true)
-    @invites = @invites.or(Friendship.where(friend_id: @user.id, status: true))
   end
 end
